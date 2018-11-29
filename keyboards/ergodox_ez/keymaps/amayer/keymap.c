687a6a68f669663698216bd5c1f149390374a3e8
Alex Mayer <amayer5125@gmail.com>
2018-11-29 16:55:26 -0500

    Add Navigation Buttons Above Arrow Keys
    Home, Page Down, Page Up, End

--- keyboards/ergodox_ez/keymaps/amayer/keymap.c
+++ keyboards/ergodox_ez/keymaps/amayer/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * | Version |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |  RST |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |      |      |      |      |      |   F12  |
+ * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |      | Home | PgUp | PgDn | End  |   F12  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |   #  |   $  |   (  |   )  |   `  |------|           |------|      | Left | Down |  Up  | Right|        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                RGB_VAD,RGB_VAI,KC_TRNS,
        // right hand
        RESET,   KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,  KC_TRNS, KC_F12,
+       KC_TRNS, KC_TRNS, KC_HOME,KC_PGDN,KC_PGUP,  KC_END,  KC_F12,
                 KC_TRNS, KC_LEFT,KC_DOWN, KC_UP,   KC_RIGHT,KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_BSLS, KC_TRNS,
                          KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
