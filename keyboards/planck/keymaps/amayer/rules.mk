27246be949198610097924f12f04cb33265ea92e
Alex Mayer <amayer5125@gmail.com>
2024-06-09 03:29:17 -0400

    Remove Unneeded Build Options and Configuration

--- keyboards/planck/keymaps/amayer/rules.mk
+++ keyboards/planck/keymaps/amayer/rules.mk
@@ -1,26 +1,5 @@
-# Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
-#
-BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration
-## (Note that for BOOTMAGIC on Teensy LC you have to use a custom .ld script.)
-MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = yes      # Audio control and System control
-CONSOLE_ENABLE = no        # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
-NKRO_ENABLE = yes          # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
-MIDI_ENABLE = no           # MIDI controls
-AUDIO_ENABLE = no          # Audio output on port C6
-UNICODE_ENABLE = no        # Unicode
-BLUETOOTH_ENABLE = no      # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
-WS2812_DRIVER = bitbang
-API_SYSEX_ENABLE = no
-ENCODER_ENABLE = yes
-DIP_SWITCH_ENABLE = yes
-ENCODER_ENABLE = no
-DIP_SWITCH_ENABLE = no
+# https://docs.qmk.fm/config_options#feature-options
+AUDIO_ENABLE = no
 CAPS_WORD_ENABLE = yes
 COMMAND_ENABLE = no
 TRI_LAYER_ENABLE = yes
